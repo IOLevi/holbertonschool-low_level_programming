922d2377142ff394b452cf2fff5b0e03fe716ea0
IOLevi <337@holbertonschool.com>
2018-01-17 23:26:05 +0000

    more changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,9 @@
  *
  * Return: 1
  */
-int main()
+int main(void)
 {
-	execl("/bin/echo", "and that piece of art is useful\" - Dora Korpar, 2015-10-19", ">&2", NULL);
+	execl("/bin/echo", "and that piece of art is useful\" - Dora Korpar,
+2015-10-19", ">&2", NULL);
 	return (1);
 }
