068e5179a0c909c647ed2f8aeca977b89db528a5
IOLevi <337@holbertonschool.com>
2018-01-18 22:49:49 +0000

    betty

--- 0x01-variables_if_else_while/7-print_tehbahpla.c
+++ 0x01-variables_if_else_while/7-print_tehbahpla.c
@@ -3,14 +3,15 @@
 /**
  * main - prints the lowercase alphabet in reverse
  *
- *Return: 0
+ * Return: 0
  */
 int main(void)
 {
 	int i;
 
-	for (i='z'; i>='a';i--)
+	for (i = 'z'; i >= 'a'; i--)
 		putchar(i);
 
 	putchar('\n');
+	return (0);
 }
