40935b47987e10ddfa1f0caa5fcd0f1a3e86943c
IOLevi <337@holbertonschool.com>
2018-01-18 23:07:29 +0000

    betty

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,15 @@ int main(void)
 {
 	int i;
 
-	for (i='0'; i<='9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
-		putchar(',');
-	        if (i != '9')
+		if (i != '9')
+		{
+			putchar(',');
 			putchar(' ');
+		}
 	}
 	putchar('\n');
+	return (0);
 }
