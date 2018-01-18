69e4ac27fdd5ed0c4f82f12508eed828dd1cca3b
IOLevi <337@holbertonschool.com>
2018-01-18 22:04:47 +0000

    betty

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,12 +3,13 @@
 /**
  * main - prints the alphabet except q and e
  *
- *Return: 0
+ * Return: 0
  */
 int main(void)
 {
 	int i;
-	for (i = 97;i < 123; i++)
+
+	for (i = 97; i < 123; i++)
 	{
 		if (i == 113 || i == 101)
 			continue;
