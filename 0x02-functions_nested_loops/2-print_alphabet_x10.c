a557fca37496e8d5046b749a564da48ff92a8b79
IOLevi <337@holbertonschool.com>
2018-01-23 21:46:27 +0000

    betty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,11 +6,11 @@
 void print_alphabet_x10(void)
 {
 	int i;
-        int j;
+	int j;
 
-	for(j = 0; j<5; j++)
+	for (j = 0; j < 5; j++)
 	{
-		for(i = 'a'; i < 'z'; i++)
+		for (i = 'a'; i < 'z'; i++)
 		{
 			_putchar(i);
 		}
