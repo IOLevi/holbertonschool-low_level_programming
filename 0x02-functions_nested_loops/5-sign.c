d5e70b824da6cd8f7752d8417ead81d544ef7b25
IOLevi <337@holbertonschool.com>
2018-01-23 22:10:31 +0000

    betty

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * print_sign -  prints the sign of a number
+ * print_sign - prints the sign of a number
  * @n: number to be eval'd
  *
  * Return: 1 if pos; 0 if 0; -1 if negative
@@ -11,19 +11,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar('\n');
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		_putchar('\n');
 		return (0);
 	}
 	else
 	{
 		_putchar('-');
-		_putchar('\n');
 		return (-1);
 	}
 }
