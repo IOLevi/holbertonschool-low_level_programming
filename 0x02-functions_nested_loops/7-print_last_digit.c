fa81f53cf043aa113b78f5dad71e7073a1fe6494
IOLevi <337@holbertonschool.com>
2018-01-23 22:14:30 +0000

    oops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
  * print_last_digit - prints the last digit of a number
+ * @n: number to be formatted
  *
- * Returns: Last digit
+ * Return: Last digit
  */
 int print_last_digit(int n)
 {
@@ -12,12 +13,12 @@ int print_last_digit(int n)
 	x = n % 10;
 	if (n < 10)
 	{
-		putchar(x * -1 + '0');
+		_putchar(x * -1 + '0');
 		return (x * -1 + '0');
 	}
 	else
 	{
-		putchar(x + '0');
+		_putchar(x + '0');
 		return (x + '0');
 	}
 }
