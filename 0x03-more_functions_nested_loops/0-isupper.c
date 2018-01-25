b3df27bd5d7ca4d724d94adc596cedf25b203553
IOLevi <337@holbertonschool.com>
2018-01-25 23:22:27 +0000

    rewrite

--- 0x03-more_functions_nested_loops/0-isupper.c
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -4,15 +4,13 @@
  * _isupper - checks for uppercase character
  * @c: char to check
  *
- * Return: returns 1 if c is upper; 0 otherwise
+ * Return: returns 1 if c is upper 0 otherwise
  */
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-
-	if ((c >= 'a') && (c <= 'z'))
+	if (c >= 'a' && c <= 'z')
 		return (1);
-
 	return (0);
 }
