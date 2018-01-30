00962714aadbff6a1b98671427f7f1a4c25e7151
IOLevi <337@holbertonschool.com>
2018-01-30 18:08:08 +0000

    ?

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -7,7 +7,10 @@
 void print_rev(char *s)
 {
 	int i;
-	int length = _strlen(s);
+	int length;
+
+	for (i = 0; s[i] != '\0'; i++)
+		length++;
 
 	for (i = length - 1; i == 0; i--)
 		_putchar(s[i]);
