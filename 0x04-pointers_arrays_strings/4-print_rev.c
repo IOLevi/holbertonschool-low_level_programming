1bb5abb724a081be2324475970bf6ca43529aa41
IOLevi <337@holbertonschool.com>
2018-01-30 17:48:45 +0000

    betty change

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,15 @@
 #include "holberton.h"
 
 /**
- * print_rev prints a string in reverse followed by a new line
- * @*s: string to be checked
+ * print_rev -  prints a string in reverse followed by a new line
+ * @s: string to be checked
  */
 void print_rev(char *s)
 {
 	int i;
-	int length;
+	int length = _strlen(s);
 
-	for (i = 0; s[i] != '\0'; i++)
-		length++;
-
-	for (i = length; i == 0; i--)
+	for (i = length - 1; i == 0; i--)
 		_putchar(s[i]);
 
 	_putchar('\n');
