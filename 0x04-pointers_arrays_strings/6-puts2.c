8979f4433f7bc277339296b419db14cdaee73bcc
IOLevi <337@holbertonschool.com>
2018-01-30 18:32:45 +0000

    ...

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -2,13 +2,17 @@
 
 /**
  * puts2 - prints one char out of 2 of a string followed by a newline
- * @*str: string to be manipulated
+ * @str: string to be manipulated
  */
 void puts2(char *str)
 {
-	int length = _strlen(str);
+	int length = 0;
+	int i;
 
-	for (i = 0; i < length; i+=2)
+	for (i = 0; str[i] != '\0'; i++)
+		length++;
+
+	for (i = 0; i < length; i += 2)
 		_putchar(str[i]);
 
 	_putchar('\n');
