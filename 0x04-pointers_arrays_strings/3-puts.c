51cc4ed93dc2c37b9f27cffe1b5e5272fbb130ef
IOLevi <337@holbertonschool.com>
2018-01-30 17:41:23 +0000

    change

--- 0x04-pointers_arrays_strings/3-puts.c
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -2,9 +2,16 @@
 
 /**
  * _puts - prints a strg follows by a newline to stdout
- * @*str: string to be printed
+ * @str: string to be printed
  */
 void _puts(char *str)
 {
-	write(stdout, str, _length(*str);
+	int length = _strlen(str);
+	int i;
+
+	for (i = 0; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\0');
 }
