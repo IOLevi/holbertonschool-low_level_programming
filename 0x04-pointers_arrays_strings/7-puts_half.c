1d4bd802544ccba9fb90eed86e83b3e49758b8d8
IOLevi <337@holbertonschool.com>
2018-01-30 18:40:06 +0000

    ..

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -2,23 +2,24 @@
 
 /**
  * puts_half - prints second half a string followed by a newline
- * @*str: string to be manipulated
+ * @str: string to be manipulated
  */
 void puts_half(char *str)
 {
-	int length = _strlen(str);
+	int length = 0;
 	int loop;
 	int i;
 
+	for (i = 0; i != '\0'; i++)
+		length++;
+
 	if (length % 2 != 0)
 		loop = (length - 1) / 2;
-        else
-                loop = length / 2;
+	else
+		loop = length / 2;
 
 	for (i = loop; i < length; i++)
 		_putchar(str[i]);
 
 	_putchar('\n');
-
-
 }
