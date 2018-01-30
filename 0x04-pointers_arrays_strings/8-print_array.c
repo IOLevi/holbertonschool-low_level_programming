b7e3744f536257f8f9c815044cd9d59ffe76820b
IOLevi <337@holbertonschool.com>
2018-01-30 19:14:20 +0000

    ...

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -3,18 +3,19 @@
 
 /**
  * print_array - prints n elements of an array followed by newline
- * @*a: array of ints
+ * @a: array of ints
  * @n: number of elements to print
  */
 void print_array(int *a, int n)
 {
-	int size = sizeof(a);
-	int elements = size / 4;
 	int i;
 
-	for (i = 0; i < elements; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", i);
-
+		if (i == n - 1)
+			printf("%d\n", a[i]);
+		else
+			printf("%d, ", a[i]);
+	}
 
 }
