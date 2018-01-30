cf31ca93ba187ed04b85142b7558fcad12b8dc4e
IOLevi <337@holbertonschool.com>
2018-01-30 17:30:14 +0000

    betty

--- 0x04-pointers_arrays_strings/2-strlen.c
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,14 @@
 
 /**
  * _strlen - returns the length of a string
- * @*s: string to be evaluated
+ * @s: string to be evaluated
  *
  * Return: the length of the string
  */
 int _strlen(char *s)
 {
 	char i;
-	int length=0;
+	int length = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 		length++;
