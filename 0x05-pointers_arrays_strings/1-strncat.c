b3abdc09d5c6975281430245f8d8036fc7279608
IOLevi <337@holbertonschool.com>
2018-02-02 01:04:40 +0000

    oops push

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -15,7 +15,7 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[destlen] != '\0')
 		destlen++;
 
-	for (i = 0; i < n & src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[destlen + i] = src[i];
 	/*dest[destlen + i] = '\0';*/
 
