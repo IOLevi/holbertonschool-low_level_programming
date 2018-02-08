2ab85a53b40cedeb8d98c1cd0fa72a0248db2ce1
IOLevi <337@holbertonschool.com>
2018-02-08 00:36:51 +0000

    betty

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -6,15 +6,24 @@
  *
  * Return: 1 if prime; 0 if not
  */
+
+int isprime(int n, int x)
+{
+
+	if (x == 1)
+		return (1);
+	if (n % x == 0)
+		return (0);
+
+	return (isprime(n, x - 1));
+
+
+}
+
 int is_prime_number(int n)
 {
-	if (n <= 0)
-		return 0;
 
-	if (n % (n - 1) == 0)
-		return 1;
-
-	return (is_prime_number(n - 1));
-
-
+	if (n < 2)
+		return (0);
+	return (isprime(n, n - 1));
 }
