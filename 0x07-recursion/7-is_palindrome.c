b79fd558069f079dfe928a89e587416c166ee6b3
IOLevi <337@holbertonschool.com>
2018-02-08 00:36:00 +0000

    betty

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -6,13 +6,30 @@
  *
  * Return: 1 if palindrome; 0 if not
  */
+
+int findlen(char *s)
+{
+	if (*s == 0)
+		return (0);
+
+	return (1 + findlen(s + 1));
+}
+
+int help(char *s, int q)
+{
+	int n;
+
+	n = findlen(s);
+	if (n == 1)
+		return (1);
+
+	if (s[0] != s[n - 1 - q])
+		return (0);
+
+	return (help(s + 1, q + 1));
+}
+
 int is_palindrome(char *s)
 {
-	if (s == '\0')
-		return 1;
-
-	return (is_palindrome(
-
-
-
+	return (help(s, 0));
 }
