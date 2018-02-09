df2212b422240bf875b4533bb085d6c2ca2b55e8
IOLevi <337@holbertonschool.com>
2018-02-09 23:00:00 +0000

    account for unused arg

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#ifdef UNUSED
+#elif defined(__GNUC__)
+# define UNUSED(x) UNUSED_ ## x __attribute__((unused))
+#elif defined(__LCLINT__)
+# define UNUSED(x) /*@unused@*/ x
+#else
+# define UNUSED(x) x
+#endif
 
+void dcc_mon_siginfo_handler(int UNUSED(whatsig));
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
