e4766a8a9e0eb4f4f90d7b59667b2764dc690b98
IOLevi <337@holbertonschool.com>
2018-02-12 04:20:07 +0000

    space

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -8,7 +8,8 @@
  *
  * Return: 0
  */
-int main(int __attribute__ ((unused)) argc, char __attribute__ ((unused)) *argv[])
+int main(int __attribute__ ((unused)) argc, char
+__attribute__ ((unused)) *argv[])
 {
 	printf("%d\n", argc - 1);
 
