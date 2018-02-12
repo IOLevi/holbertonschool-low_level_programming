fa93b909208759352433c501b64d80e7e62e4388
IOLevi <337@holbertonschool.com>
2018-02-12 03:59:50 +0000

    betty

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+/**
+ * main - prints program name
+ * @argc: number of args
+ * @argv: array of char pointers
+ *
+ * Return: 0
+ */
 int main(int __attribute__ ((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
