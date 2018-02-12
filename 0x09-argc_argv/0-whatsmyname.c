96dd4c0605d06e08ee45456f6727a93025203ed6
IOLevi <337@holbertonschool.com>
2018-02-12 03:58:34 +0000

    change unused

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
-#ifdef UNUSED
-#elif defined(__GNUC__)
-# define UNUSED(x) UNUSED_ ## x __attribute__((unused))
-#elif defined(__LCLINT__)
-# define UNUSED(x) /*@unused@*/ x
-#else
-# define UNUSED(x) x
-#endif
+#include <stdlib.h>
 
-void dcc_mon_siginfo_handler(int UNUSED(whatsig));
-int main(int argc, char *argv[])
+
+int main(int __attribute__ ((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
