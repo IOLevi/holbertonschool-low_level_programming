621f40bb6e48c43312340767a600740a1d968d00
IOLevi <337@holbertonschool.com>
2018-02-13 23:37:02 +0000

    test

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -20,7 +20,7 @@ int **alloc_grid(int width, int height)
 	if (!p)
 		return (NULL);
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
 		p[i] = (int *)malloc(sizeof(int) * width);
 		if (p[i] == NULL)
