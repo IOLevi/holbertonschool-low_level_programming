37cf921f250a3f29b74d8f4a24586f153a72865c
IOLevi <337@holbertonschool.com>
2018-02-15 20:54:45 +0000

    first commit

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * void *_calloc - duplicate calloc functionality
+ * @nmemb: number of elements
+ * @size: size of a single element
+ *
+ * Return: a pointer to the array, all intialized to zero; null if failure
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	int i;
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+	if (!p)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		*(p + i) = 0;
+
+	return (p);
+}
