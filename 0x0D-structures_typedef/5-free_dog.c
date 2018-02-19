1a5dd8da3d0c2193b78c0578f8cc1ba3894bf5dd
IOLevi <337@holbertonschool.com>
2018-02-19 23:19:49 +0000

    error changes

--- 0x0D-structures_typedef/5-free_dog.c
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -1,11 +1,14 @@
-#include "dog"
-
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * free_dog - frees the doge from the heap
  * @d: pointer to dog that needs to be free
  */
 void free_dog(dog_t *d)
 {
+	if (!d)
+		return;
 	free(d->name);
 	free(d->owner);
 	free(d);
