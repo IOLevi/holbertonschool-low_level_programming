f5c8740b12989c0f08734575fdb671af052c67f1
IOLevi <337@holbertonschool.com>
2018-02-19 23:40:47 +0000

    changed order

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 
-	printf("AGE: %.6f\n", d->age);
+	printf("AGE: %f\n", d->age);
 
 	if (d->owner != NULL)
 		printf("Owner: %s\n", d->owner);
