30576ca33316ae256289765a279e459b7b73148b
IOLevi <337@holbertonschool.com>
2018-02-19 23:29:21 +0000

    removed age error checking

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -10,15 +10,13 @@ void print_dog(struct dog *d)
 {
 	if (!d)
 		return;
+
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 
-	if (d->age != 0)
-		printf("AGE: %.6f\n", d->age);
-	else
-		printf("AGE: (nil)\n");
+	printf("AGE: %.6f\n", d->age);
 
 	if (d->owner != NULL)
 		printf("Owner: %s\n", d->owner);
