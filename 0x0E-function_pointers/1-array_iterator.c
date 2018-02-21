15168996817dc407cd0093c67d0514bae3434379
IOLevi <337@holbertonschool.com>
2018-02-21 23:55:42 +0000

    betty changes

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 /**
- * array_interator - write a function that executes a function given as a param on each element of array
+ * array_iterator - write a function that executes a function on an array
+ * @array: array of ints
  * @size: size of the array
  * @action: pointer to the function to use
  */
