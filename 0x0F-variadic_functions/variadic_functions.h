a26ece60e1b90176390ca72a2f397effed14b44b
IOLevi <337@holbertonschool.com>
2018-02-25 23:09:29 +0000

    test

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef CESTCOOL
 #define CESTCOOL
+#include <stdarg.h>
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
