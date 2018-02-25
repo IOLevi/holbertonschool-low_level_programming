0e76e936b30ffae5e9c5de3fd19a5849ffed5f5c
IOLevi <337@holbertonschool.com>
2018-02-25 19:47:46 +0000

    first commit

---
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef CESTCOOL
+#define CESTCOOL
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+typedef struct{
+	char *s;
+	void (*f)(va_list);
+} Match;
+#endif
