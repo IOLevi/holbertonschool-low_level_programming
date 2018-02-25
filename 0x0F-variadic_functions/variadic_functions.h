dcfa8b5c0b44e8a3daac6ffb8be8e608e9f1e4c2
IOLevi <337@holbertonschool.com>
2018-02-25 23:11:05 +0000

    no

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -6,7 +6,12 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-typedef struct{
+/**
+ * struct test - this is a struct
+ * @s: test
+ * @f: test
+ */
+typedef struct test{
 	char *s;
 	void (*f)(va_list);
 } Match;
