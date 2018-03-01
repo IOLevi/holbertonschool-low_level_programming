19d5e83f2951bcf27e1da5b9c2e9ec4620ce5276
IOLevi <337@holbertonschool.com>
2018-03-01 22:10:08 +0000

    whitespace

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -13,15 +13,14 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str != NULL)
-			printf("[%u] %s\n", h->len, h->str); 
+			printf("[%u] %s\n", h->len, h->str);
 		else
 			printf("[0] (nil)\n");
 		n++;
-		h = h->next; 
+		h = h->next;
 	}
 
 	return (n);
 
 
 }
-
