a0d5f3fbbb23152eb0e8aa4c1fffb9c1a180cc5f
IOLevi <337@holbertonschool.com>
2018-03-01 22:18:35 +0000

    test

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -5,11 +5,14 @@
  */
 void free_list(list_t *head)
 {
+	list_t *tmp;
+
 	while (head != NULL)
 	{
+		tmp = head->next;
 		free(head->str);
 		free(head);
-		head = head->next;
+		head = tmp;
 	}
 
 }
