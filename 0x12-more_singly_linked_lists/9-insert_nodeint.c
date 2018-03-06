a20cf76d45a4d609f7782682ce7161d33a603f74
IOLevi <337@holbertonschool.com>
2018-03-06 18:52:38 +0000

    test

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *hpt, *tptr, *lev;
+	listint_t *hptr, *tptr, *lev;
 	unsigned int i;
 
 	if (head == NULL)
