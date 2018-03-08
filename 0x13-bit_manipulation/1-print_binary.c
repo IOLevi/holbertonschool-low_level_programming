5af7240efbbbb7fdb367d388e940fb44d82646e3
IOLevi <337@holbertonschool.com>
2018-03-08 22:13:40 +0000

    betty

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -14,10 +14,10 @@ void print_binary(unsigned long int n)
 
 	while (mask > 0)
 	{
-		if ((n & mask) == 0 )
+		if ((n & mask) == 0)
 			_putchar('0');
 		else
 			_putchar('1');
-		mask = mask >> 1 ;
+		mask = mask >> 1;
 	}
 }
