c0de9a87640428195307e8595f31059ceb367a37
IOLevi <337@holbertonschool.com>
2018-04-15 13:37:52 -0700

    change type of counter to unsigned

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -8,12 +8,12 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	int i;
+	unsigned int i;
 
 	/*empty list*/
 	if (!head)
 		return (NULL);
-	
+
 	for (i = 0; i < index; i++)
 	{
 		/*index out of range*/
