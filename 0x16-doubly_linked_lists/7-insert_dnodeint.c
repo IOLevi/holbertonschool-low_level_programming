6674b722f7d4de314a8deb1c6cd999f72b0b0fbd
IOLevi <337@holbertonschool.com>
2018-04-15 14:12:25 -0700

    betty style

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,29 +11,29 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *hpointer, *new;
 	size_t len;
-	int i;
+	unsigned int i;
 
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
-	new->previous = NULL;
+	new->prev = NULL;
 	new->next = NULL;
 	new->n = n;
 
-	hpointer =*h;
+	hpointer = *h;
 
 	/*empty list or add to beginning*/
 	if (!hpointer || idx == 0)
-		return(add_dnodeint(h)); //h because it needs to update head
+		return (add_dnodeint(h, n)); /*h because it needs to update head*/
 
 	len = dlistint_len(hpointer);
-	
+
 	/*idx at end of list*/
 	if (len == idx)
 	{
-		return(add_dnodeint_end(h));
+		return (add_dnodeint_end(h, n));
 	}
-	
+
 	/*idx out of range */
 	if (idx > len)
 	{
@@ -46,13 +46,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		hpointer = hpointer->next;
 	}
 
-	// 0 new 1 2 3
-	// hpointer points to 1
-
 	new->prev = hpointer->prev;
 	new->next = hpointer;
-	hpointer->previous->next = new;
-	hpointer->previous = new;
+	hpointer->prev->next = new;
+	hpointer->prev = new;
 
 	return (new);
 }
