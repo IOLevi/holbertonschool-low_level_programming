536b84bb168f618dc8dccef807a15c7c1bed4efe
IOLevi <337@holbertonschool.com>
2018-05-10 11:22:50 -0700

    first commit

---
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new;
+	
+	new = malloc(sizeof(hash_table_t));
+	if (!new)
+		return (NULL);
+	new->size = size;
+	new->array = malloc(sizeof(hash_node_t) * size);
+	if (!new->array)
+		return (NULL);
+	return new;
+}
