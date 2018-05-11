0a9f4dc1852ab646037ca9bb5134cfa2870007b7
IOLevi <337@holbertonschool.com>
2018-05-11 19:23:44 +0000

    added checks for nulls

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -11,6 +11,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int ki;
 	hash_node_t *temp;
 
+	if (!ht)
+		return (NULL);
+	if (!key)
+		return (NULL);
+
 	ki = key_index((const unsigned char *)key, ht->size);
 
 	if (ht->array[ki] == NULL)
