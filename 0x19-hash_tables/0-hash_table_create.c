2920d3dbd77bde83313a0579116889f20d251132
IOLevi <337@holbertonschool.com>
2018-05-11 21:02:52 +0000

    association of bang

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: of buckets
- * Return: pointer to the table
+ * hash_table_create - aergaegea
+ * @size: gragrea
+ * Return: gruehgera 
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -12,17 +12,21 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
+
 	new = malloc(sizeof(hash_table_t));
 	if (!new)
 		return (NULL);
+
+	new->size = size;
 	new->array = malloc(sizeof(hash_node_t *) * size);
-	if (!new->array)
+	if (!(new->array))
 	{
 		free(new);
 		return (NULL);
 	}
+
 	for (i = 0; i < size; i++)
 		new->array[i] = NULL;
-	new->size = size;
+
 	return (new);
 }
