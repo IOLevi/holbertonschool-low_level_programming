c89c520cf3a8ec6e0857dc4edf3ffebdc337ab6b
IOLevi <337@holbertonschool.com>
2018-05-11 20:52:30 +0000

    account for size 0

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -10,6 +10,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new = NULL;
 	unsigned long int i;
 
+	if (size == 0)
+		return (NULL);
 	new = malloc(sizeof(hash_table_t));
 	if (!new)
 		return (NULL);
