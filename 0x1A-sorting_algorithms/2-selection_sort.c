baf8a806a7f291757c32ad1ff6b8d42ed7e3d3da
IOLevi <337@holbertonschool.com>
2018-05-24 22:42:42 -0700

    another

---
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,33 @@
+
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j;
+	size_t smallest;
+	size_t idxsmall;
+	size_t k;
+	int temp;
+
+	smallest = array[0];
+	//outer loop of size n
+	for (i = 0; i < size; i++)
+	{
+		//find the smallest in the unsorted set
+		smallest = array[i];
+		idxsmall = i;
+		for (j = i; j < size; j++)
+		{
+			if (array[j] < smallest)
+			{
+				smallest = array[j]
+				idxsmall = j;
+			}
+		}
+
+		//swap array[idxsmall] with array[i]
+		temp = array[i];
+		array[i] = array[idxsmall];
+		array[idxsmall] = temp;
+	}
+}
