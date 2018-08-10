bd2d3ebed30106ecce178b6570bcab95a81de204
IOLevi <337@holbertonschool.com>
2018-08-09 21:03:41 -0700

    fixed output line

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -11,12 +11,13 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 	/* print the value each time */
+
 	if (!array || size == 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked at array[%lu] = [%d]\n", (long int)i, array[i]);
+		printf("Value checked array[%lu] = [%d]\n", (long int)i, array[i]);
 
 		if (array[i] == value)
 			return ((int) i);
